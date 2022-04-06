ef17b1515aff1cb4a2512149dd799e091dd6936a
Muhammad Asif <asif88383@gmail.com>
2022-04-06 12:46:09 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setJobs(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if(loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const {company, dates, duties, title} = jobs[value];
 }
 
 export default App
